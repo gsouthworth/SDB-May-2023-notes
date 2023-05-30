657ed293b935c6cf2a3966e38bd64a7cee98e6a7
Gavin Southworth <gavinsouthworth@Gavins-Air.home>
2023-05-30 09:25:54 -0400

    unit-2 added

---
+++ unit-2/data-types.js
@@ -0,0 +1,9 @@
+/* 
+    ? JavaScript
+        - Founded in 1996
+        * Client-side language (runs in the browser)
+        * Interpreted language not compiled
+        * First-class function, functions are treated as any other variables
+        * functional language
+        * object oriented language
+*/
